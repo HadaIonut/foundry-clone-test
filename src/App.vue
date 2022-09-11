00263f53d3a4a8f84196275784238d1327b86631
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2022-09-11 17:25:21 +0300

    draggable light source and gitignore

--- src/App.vue
+++ src/App.vue
@@ -6,103 +6,99 @@ import {DragControls} from "three/addons/controls/DragControls.js";
 
 const app = ref(null)
 
-let camera, scene, renderer, controls ,mesh, light, lightControls, sphere;
+let camera, scene, renderer, controls, mesh, light, lightControls, sphere;
 
-const createWorld = () => {
-  const wallGroup = new THREE.Group();
-  const wallObject = new THREE.Object3D();
+const initGround = () => {
+    const groundTexture = new THREE.TextureLoader().load('./map.jpg')
+    console.log(groundTexture.width)
+    const ground = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), new THREE.MeshStandardMaterial({
+        map: groundTexture
+    }));
+    ground.rotateX(-Math.PI / 2);
+    ground.castShadow = true;
+    ground.receiveShadow = true;
 
-  wallGroup.add(wallObject);
-  scene.add(wallGroup);
+    scene.add(ground)
+}
 
-  // Draw side walls
-  const wallShape = new THREE.Shape();
-  wallShape.moveTo(0, 0);
-  wallShape.lineTo(500, 0);
-  wallShape.lineTo(500, 35);
-  wallShape.lineTo(0, 35);
-  wallShape.lineTo(0, 0);
-  const wallGeometry = new THREE.ExtrudeGeometry([ wallShape ], {
-    steps: 1,
-    depth: 1,
-    bevelEnabled: false,
-    curveSegments: 32
-  });
-  const wallA = new THREE.Mesh(wallGeometry, new THREE.MeshStandardMaterial({ color: 0xff9999}));
-  wallA.rotateY(-Math.PI / 2);
-  wallA.translateX(-200);
-  wallA.castShadow = true;
-  wallA.receiveShadow = true;
-  wallObject.add(wallA);
+const initLights = () => {
+    light = new THREE.PointLight(0xffffff, 10, 2000);
+    light.position.set(0, 10, 0);
+    light.castShadow = true;
+    scene.add(light);
 
-  const wallB = wallA.clone();
-  wallB.translateZ(-150);
-  wallObject.add(wallB);
+    const geometry = new THREE.SphereGeometry(20);
+    const material = new THREE.MeshBasicMaterial({color: "orange", emissive: 0.5});
+    const cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = false;
+    scene.add(cube)
 
-  const groundTexture = new THREE.TextureLoader().load( './map.jpg' )
-  
-  const ground = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), new THREE.MeshStandardMaterial({
-    map: groundTexture
-  }));
-  ground.rotateX(-Math.PI / 2);
-  ground.castShadow = true;
-  ground.receiveShadow = true;
-  wallGroup.add(ground);
+    const controls = new DragControls([cube], camera, renderer.domElement);
 
-  light = new THREE.PointLight( 0xffffff, 5, 1000);
-  light.position.set( 70, 10, 1 );
-  light.castShadow = true
-  scene.add( light );
+    controls.addEventListener('drag', function (event) {
+        light.position.x = cube.position.x;
+        light.position.z = cube.position.z;
+    });
 
-  mesh = [ wallGroup, wallObject ];
+}
 
+const initWalls = () => {
+    const wall1 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 200), new THREE.MeshBasicMaterial({color: "red"}));
+    wall1.castShadow = true
+    // wall1.receiveShadow = true
+    wall1.position.x = -100;
+    scene.add(wall1)
+    const wall2 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 400), new THREE.MeshBasicMaterial({color: "blue"}));
+    wall2.castShadow = true;
+    // wall2.receiveShadow = true;
+    wall2.position.x = 100;
+    scene.add(wall2)
 };
 
+
 const init = () => {
-  // camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000.0);
-  camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 0.1, 10000 );
-  camera.position.set(0,1,0)
-  // camera.zoom = 0.5
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000.0);
+    // camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000);
+    camera.position.set(0, 700, 0)
+    // camera.zoom = 0.5
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x333333);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x333333);
 
-  // scene.add(new THREE.HemisphereLight(0xffffcc, 0x19bbdc, 1));
+    // scene.add(new THREE.HemisphereLight(0xffffcc, 0x19bbdc, 1));
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // renderer.shadowMap.type = THREE.VSMShadowMap
-  renderer.shadowMap.enabled = true
-  controls = new OrbitControls( camera, renderer.domElement );
-  controls.enableRotate = false
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.shadowMap.enabled = true
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableRotate = false
 
-  watch(app, (newValue) => {
-    if (!newValue) return
+    watch(app, (newValue) => {
+        if (!newValue) return
 
-    newValue.appendChild(renderer.domElement)
-  })
+        newValue.appendChild(renderer.domElement)
+    })
 
-  createWorld();
+    initGround();
+    initLights();
+    initWalls();
 }
 
 const animate = () => {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  // controls.update();
-  // lightControls.update()
-
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
 init();
 animate();
-
 </script>
 
 <template>
-  <div ref="app">
+    <div ref="app">
 
-  </div>
+    </div>
 </template>
 
 <style scoped>
