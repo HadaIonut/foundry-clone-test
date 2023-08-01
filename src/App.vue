45004cf4637c86c0060a10889fa0b6b64c5cf300
ionut <hadaionut@gmail.com>
2023-08-01 14:24:50 +0300

    optimiezed shadow calculation for large number of lights

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,37 @@ import {ref, watch} from "vue";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import {DragControls} from "three/addons/controls/DragControls.js";
 import {ConvexGeometry} from "three/addons/geometries/ConvexGeometry.js";
+import Stats from 'stats.js'
+import {ProgressiveLightMap} from "three/addons/misc/ProgressiveLightMap.js";
+import {PCFSoftShadowMap} from "three";
 
 const app = ref(null)
 
-let camera, scene, renderer, controls, mesh, light, lightControls, sphere, line, transformTest;
+let camera, scene, renderer, controls, mesh, lightControls, sphere, line, transformTest;
 let isBuilding = false;
 let mouseMoveCoords = []
 let points = []
 let click = []
+let progressiveSurfacemap;
 
 let walls = [];
 let wallWorkingIndex = 0;
+const params = { 'Enable': true, 'Blur Edges': true, 'Blend Window': 200,
+    'Light Radius': 50, 'Ambient Weight': 0.5, 'Debug Lightmap': false };
+
+const stats = new Stats()
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
 
 const initGround = () => {
     const groundTexture = new THREE.TextureLoader().load('./map.jpg')
-    console.log(groundTexture.width)
-    const ground = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), new THREE.MeshStandardMaterial({
-        map: groundTexture
-    }));
+    const ground = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000),
+        new THREE.MeshPhongMaterial({
+            map: groundTexture,
+            side: THREE.DoubleSide,
+            color: 0xffffff, depthWrite: true
+        }));
     ground.rotateX(-Math.PI / 2);
-    ground.castShadow = true;
     ground.receiveShadow = true;
 
     scene.add(ground)
@@ -31,20 +42,27 @@ const initGround = () => {
     const divisions = 20;
 
     const gridHelper = new THREE.GridHelper(size, divisions);
+    gridHelper.position.y += 5;
     scene.add(gridHelper);
 }
 
-const initLights = () => {
-    light = new THREE.PointLight(0xffffff, 10, 2000);
-    light.position.set(0, 10, 0);
+const initLights = ({x, y, z}) => {
+    const light = new THREE.PointLight(0xffffff, 1000, 500, 1);
+    light.position.set(x, y, z);
     light.castShadow = true;
+
     scene.add(light);
+    console.log(light.visible)
 
     const geometry = new THREE.SphereGeometry(20);
-    const material = new THREE.MeshBasicMaterial({color: "orange", emissive: 0.5});
+    const material = new THREE.MeshBasicMaterial({color: "orange"});
     const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(x, y, z)
     cube.castShadow = false;
     scene.add(cube)
+
+    const helper = new THREE.PointLightHelper(light);
+    scene.add(helper);
 
     const controls = new DragControls([cube], camera, renderer.domElement);
 
@@ -58,27 +76,35 @@ const initLights = () => {
     controls.addEventListener("dragend", (event) => {
         const grid = 50;
         const halfGrd = grid / 2;
-        cube.position.x = Math.round((cube.position.x + halfGrd) / grid) * grid - halfGrd;
-        cube.position.z = Math.round((cube.position.z + halfGrd) / grid) * grid - halfGrd;
-        light.position.x = cube.position.x;
-        light.position.z = cube.position.z;
+        cube.position.set(
+            Math.round((cube.position.x + halfGrd) / grid) * grid - halfGrd,
+            cube.position.y,
+            Math.round((cube.position.z + halfGrd) / grid) * grid - halfGrd
+        )
+
+        light.position.set(cube.position.x, cube.position.y, cube.position.z)
+        renderer.shadowMap.needsUpdate = true
+        // light.position.x = cube.position.x;
+        // light.position.z = cube.position.z;
+        // light.needsUpdate = true
     })
 
 }
 
 const initWalls = () => {
-    const wall1 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 200), new THREE.MeshBasicMaterial({color: "red"}));
+    const wall1 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 200), new THREE.MeshPhongMaterial({ color: 0xffffff, depthWrite: true }));
     console.log(wall1)
     wall1.castShadow = true
-    // wall1.receiveShadow = true
+    wall1.receiveShadow = true
     wall1.position.x = -100;
     scene.add(wall1)
-    const wall2 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 400), new THREE.MeshBasicMaterial({color: "blue"}));
+    const wall2 = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 400), new THREE.MeshPhongMaterial({ color: 0xffffff, depthWrite: true }));
     wall2.castShadow = true;
-    // wall2.receiveShadow = true;
+    wall2.receiveShadow = true;
     wall2.position.x = 100;
     scene.add(wall2)
-    console.log(wall2)
+    progressiveSurfacemap.addObjectsToLightMap( walls );
+
 };
 
 const findLocationFromCoords = (x, y) => {
@@ -111,7 +137,6 @@ const initLine = (x, y) => {
     line.castShadow = true
 
     scene.add(line);
-    console.log(line.geometry.attributes.position.array)
     isBuilding = true;
 }
 
@@ -161,11 +186,17 @@ const testShape = (x, y) => {
     scene.add(walls[wallWorkingIndex])
 }
 
+const getRandomInt = (max) => {
+    return Math.ceil(Math.random() * max) * (Math.round(Math.random()) ? 1 : -1)
+}
+
 const init = () => {
-    // camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000.0);
+    // camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000.0);
     camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000);
     camera.position.set(0, 700, 0)
     // camera.zoom = 0.5
+
+    progressiveSurfacemap = new ProgressiveLightMap( renderer, 256 );
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x333333);
@@ -174,17 +205,19 @@ const init = () => {
 
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
     renderer.shadowMap.enabled = true
+
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableRotate = false // TODO FACI TU DACA VREI SA POTI ROTI CAMERA
+
 
     watch(app, (newValue) => {
         if (!newValue) return
 
         newValue.appendChild(renderer.domElement)
         newValue.addEventListener('click', (event) => {
-            testShape(event.clientX, event.clientY)
+            if (event.altKey) testShape(event.clientX, event.clientY)
         })
     })
 
@@ -194,12 +227,15 @@ const init = () => {
     })
 
     initGround();
-    initLights();
+    for (let i = 0; i < 25; i++) {
+        initLights({x: getRandomInt(500), y: 10, z: getRandomInt(500)});
+    }
     initWalls();
     testShape()
 }
 
 const animate = () => {
+    stats.begin()
     requestAnimationFrame(animate);
 
     const newMouseCoords = findLocationFromCoords(mouseMoveCoords[0], mouseMoveCoords[1])
@@ -207,7 +243,6 @@ const animate = () => {
     if (newMouseCoords.length !== 0 && walls[wallWorkingIndex]) {
         const angle = Math.atan(Math.abs(points[0].z - newMouseCoords[1]) / Math.abs(points[0].x - newMouseCoords[0])) || 0;
         // TODO: REFACTORIZAM AICI MATEMATICA SAU IGNOIRAM CA SAR CAPETELE PERETIILOR
-        console.log("angle", angle, "rad");
         const widthX = Math.sin(angle) * 5;
         const widthY = Math.cos(angle) * 5 * (((globalClick.z > newMouseCoords[1] && globalClick.x > newMouseCoords[0]) || (globalClick.z < newMouseCoords[1] && globalClick.x < newMouseCoords[0])) ? -1 : 1);
 
@@ -236,8 +271,20 @@ const animate = () => {
         walls[wallWorkingIndex].geometry.attributes.position.needsUpdate = true
     }
 
+    if ( params[ 'Enable' ] ) {
+
+        progressiveSurfacemap.update( camera, params[ 'Blend Window' ], params[ 'Blur Edges' ] );
+
+        if ( ! progressiveSurfacemap.firstUpdate ) {
+
+            progressiveSurfacemap.showDebugLightmap( params[ 'Debug Lightmap' ] );
+
+        }
+
+    }
 
     renderer.render(scene, camera);
+    stats.end()
 }
 
 init();
