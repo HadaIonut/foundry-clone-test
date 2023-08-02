60ee59b75c6ebd733bea59a84965e92a7da8e454
ionut <hadaionut@gmail.com>
2023-08-02 15:14:18 +0300

    added moving of dynamic object

--- src/adjustableShape.js
+++ src/adjustableShape.js
@@ -5,16 +5,16 @@ import {Vector3} from "three";
 let centralPoint;
 
 const findCenterOfObject = (points) => {
-    const xCenter = points.reduce((acc, cur) => acc + cur.x ,0) / points.length
-    const zCenter = points.reduce((acc, cur) => acc + cur.z ,0) / points.length
+    const xCenter = points.reduce((acc, cur) => acc + cur.x, 0) / points.length
+    const zCenter = points.reduce((acc, cur) => acc + cur.z, 0) / points.length
 
     return new Vector3(xCenter, 0, zCenter)
 }
 
 const createCenterPoint = (controlPoints, scene) => {
-    const points = controlPoints.reduce((acc, cur) => [...acc, cur.position],[])
+    const points = controlPoints.reduce((acc, cur) => [...acc, cur.position], [])
 
-    centralPoint = createPoint(findCenterOfObject(points), scene)
+    centralPoint = createPoint(findCenterOfObject(points), scene, 'blue', 'centerPoint')
 }
 
 const createCurveGeometry = (controlPoints, tension) => {
@@ -22,7 +22,7 @@ const createCurveGeometry = (controlPoints, tension) => {
     controlPoints.forEach(pt => {
         pts.push(pt.position);
     });
-    const curve = new THREE.CatmullRomCurve3( pts, true, 'catmullrom', tension.value );
+    const curve = new THREE.CatmullRomCurve3(pts, true, 'catmullrom', tension.value);
     curve.closed = true;
     const curveGeometry = new THREE.BufferGeometry();
     curveGeometry.vertices = curve.getPoints(75);
@@ -34,26 +34,27 @@ const createCurveGeometry = (controlPoints, tension) => {
     return curveGeometry;
 }
 
-export const createPoint = (position, scene, color = 'white' ) => {
+export const createPoint = (position, scene, color = 'white', objectName = '') => {
     const viewGeometry = new THREE.BoxGeometry(15, 50, 15, 1, 3, 1);
     viewGeometry.translate(0, .75, 0);
     const viewMaterial = new THREE.MeshBasicMaterial({color: color, wireframe: false, transparent: true, opacity: .5});
     const view = new THREE.Mesh(viewGeometry, viewMaterial);
     view.position.copy(position);
+    view.name = objectName
     scene.add(view);
     return view;
 }
-export const adjustableShape = (scene, controls, rayCaster, controlPoints, plane, mouse, tension ) => {
+export const adjustableShape = (scene, controls, rayCaster, controlPoints, plane, mouse, tension) => {
     createCenterPoint(controlPoints, scene)
 
     const curveMaterial = new THREE.LineBasicMaterial({color: "white"});
     const curveLine = new THREE.Line(createCurveGeometry(controlPoints, tension), curveMaterial);
 
-    const extrudeSettings = { amount: 1, bevelEnabled: false, depth: 20};
+    const extrudeSettings = {amount: 1, bevelEnabled: false, depth: 20};
     const points = [];
     let shape = null;
     let shapeGeometry;
-    const shapeMaterial = new THREE.MeshBasicMaterial({color:'red'})
+    const shapeMaterial = new THREE.MeshBasicMaterial({color: 'red'})
     const shapeMesh = new THREE.Mesh(shapeGeometry, shapeMaterial);
 
     let intersects;
@@ -72,11 +73,11 @@ export const adjustableShape = (scene, controls, rayCaster, controlPoints, plane
     shapeMesh.castShadow = true
     scene.add(shapeMesh);
     scene.add(curveLine);
-    curveLine.geometry.vertices.forEach((vertex, index)=>{
+    curveLine.geometry.vertices.forEach((vertex, index) => {
         points.push(new THREE.Vector2(vertex.x, vertex.z)); // fill the array of points with THREE.Vector2() for re-use
     });
     const extrudeMesh = () => {
-        curveLine.geometry.vertices.forEach((vertex, index)=>{
+        curveLine.geometry.vertices.forEach((vertex, index) => {
             points[index].set(vertex.x, vertex.z); // re-use the array
         });
 
@@ -90,14 +91,20 @@ export const adjustableShape = (scene, controls, rayCaster, controlPoints, plane
     extrudeMesh();
 
     const onMouseDown = () => {
-        intersects = rayCaster.intersectObjects(controlPoints);
-        if (intersects.length > 0){
+        const controlPointsIntersection = rayCaster.intersectObjects(controlPoints)
+        const centralPointIntersection = rayCaster.intersectObject(centralPoint);
+
+        if (controlPointsIntersection.length) intersects = controlPointsIntersection
+        else intersects = centralPointIntersection
+
+        if (intersects.length > 0) {
             controls.enableRotate = false;
             dragObject = intersects[0].object;
             plane.setFromNormalAndCoplanarPoint(planeNormal, intersects[0].point);
             shift.subVectors(dragObject.position, intersects[0].point);
             dragging = true;
         }
+
     }
 
     const onMouseUp = () => {
@@ -107,14 +114,33 @@ export const adjustableShape = (scene, controls, rayCaster, controlPoints, plane
     }
 
     const onMouseMove = (event) => {
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
         if (intersects?.length === 0 || !dragging) return;
-        rayCaster.ray.intersectPlane(plane, pointOfIntersection);
-        dragObject.position.copy(pointOfIntersection).add(shift);
-        curveLine.geometry.dispose();
-        curveLine.geometry = createCurveGeometry(controlPoints, tension);
-        extrudeMesh();
+
+        if (intersects[0].object.name === 'centerPoint') {
+            const oldObjectPosition = dragObject.position.clone()
+            rayCaster.ray.intersectPlane(plane, pointOfIntersection);
+            dragObject.position.copy(pointOfIntersection).add(shift);
+            const newObjectPosition = dragObject.position
+
+            const moveDelta = new Vector3().subVectors(newObjectPosition, oldObjectPosition)
+
+            controlPoints.forEach((point, index) => {
+                point.position.setY(25).add(moveDelta)
+            })
+
+            curveLine.geometry.dispose();
+            curveLine.geometry = createCurveGeometry(controlPoints, tension);
+            extrudeMesh();
+        } else {
+            rayCaster.ray.intersectPlane(plane, pointOfIntersection);
+            dragObject.position.copy(pointOfIntersection).add(shift);
+            curveLine.geometry.dispose();
+            curveLine.geometry = createCurveGeometry(controlPoints, tension);
+            extrudeMesh();
+        }
+
     }
 
     window.addEventListener("mousedown", onMouseDown, false);
